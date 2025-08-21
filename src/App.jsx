02ba5c719b40d8bbef7411896291d393c05b41ac
Peter <skwortsow.petia@yandex.ru>
2025-08-21 19:05:31 +0300

    Начал с начала

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+
 import "./App.css";
-import { Canvas } from "@react-three/fiber";
-import Experience from "./components/Experience";
-import Configurator from "./components/Configurator";
-import { CustomizationProvider } from "./contexts/Customization";
+
 
 function App() {
   return (
-    <CustomizationProvider>
       <div className="App">
-        <Canvas dpr={[1, 2]}>
-          <color attach="background" args={["#213547"]} />
-          <fog attach="fog" args={["#213547", 10, 20]} />
-          <Experience />
-        </Canvas>
-        <Configurator />
+          <h1>Product Customization</h1>
       </div>
-    </CustomizationProvider>
   );
 }
 
